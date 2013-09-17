2f9ba2326e64a95232d2ede60f047a679143de93
Debajyoti Nag <dave0908@gmail.com>
2013-09-17 18:05:02 +0200

    updated whitewashing scripts and README

--- scripts/whitewashing_algo/src/clipping_algo_src/read_image_info.py
+++ scripts/whitewashing_algo/src/clipping_algo_src/read_image_info.py
@@ -1,42 +1,71 @@
 from PIL import Image, ImageDraw
 import csv as csv
+import sys
+import os
 
-#open image and convert to hsv if needed
-location  = "D:\\Dropbox\\ankur\\pixel_tests\\"  #file location
-imFile = "test.png"
-im = Image.open(location + imFile)
-if im.mode != "L" :
-    im2 = im.convert(mode = "L") 
-else
-	im2 = im
-#print im2.mode
+def read_info(location , imFile) :
+    #open image and convert to hsv if needed
+    
+    im = Image.open(location + imFile)
+    if im.mode != "L" :
+        im2 = im.convert(mode = "L") 
+    else:
+        im2 = im
+    #print im2.mode
+    
+    nx,ny =  im2.size
+    #whitePixel = []
+    blackPixel = []
+    
+    i = 1
+    #check the pixels
+    for y in range(ny):
+        whiteCount = 0
+        blackCount = 0
+        for x in range(nx):
+            xy = (x,y)
+            pix = im2.getpixel(xy)
+            if pix == 255: #255 is for white
+                #whitePixel.append(xy)
+                whiteCount += 1
+                #im2.putpixel(xy,1) #used to crudely invert the image - required for testing purposes
+            else :
+                blackCount += 1
+                #blackPixel.append(xy)
+        if blackCount >= (0.05 *nx):
+            blackPixel.append((i,y,blackCount))
+            i = i +1
+    
+    file = csv.writer(open(location + "test.csv","wb"))
+    
+    for each in blackPixel:
+        file.writerow(each)
 
-nx,ny =  im2.size
-#whitePixel = []
-blackPixel = []
 
-i = 1
-#check the pixels
-for y in range(ny):
-    whiteCount = 0
-    blackCount = 0
-    for x in range(nx):
-        xy = (x,y)
-        pix = im2.getpixel(xy)
-        if pix == 255: #255 is for white
-            #whitePixel.append(xy)
-            whiteCount += 1
-            #im2.putpixel(xy,1) #used to crudely invert the image - required for testing purposes
-        else :
-            blackCount += 1
-            #blackPixel.append(xy)
-    if blackCount >= (0.05 *nx):
-        blackPixel.append((i,y,blackCount))
-        i = i +1
-
-file = csv.writer(open(location + "test.csv","wb"))
-
-for each in blackPixel:
-    file.writerow(each)
-
-
+if __name__ == '__main__':
+    
+    #default location
+    location  = "D:\\Dropbox\\ankur\\pixel_tests\\"  #file location
+    imFile = "test.png"
+    
+    #user input for location
+    #if(len(sys.argv)!=5):
+    #    print "Usage: python read_image_info.py -location <location> -imFile <file name>"
+    #    print "Error : insufficient arguments"
+    #    exit()
+    #
+    #if(sys.argv[1]=="-location"):
+    #    location=sys.argv[2]
+    #else:
+    #    print "Usage: python read_image_info.py -location <location> -imFile <file name>"
+    #    print "Error : wrong location"
+    #    exit()
+    #
+    #if(sys.argv[3]=="-imFile"):
+    #    imFile=sys.argv[4]
+    #else:
+    #    print "Usage: python read_image_info.py -location <location> -imFile <file name>"
+    #    print "Error : wrong filename"
+    #    exit()
+    
+    read_info(location,imFile)
--- scripts/whitewashing_algo/src/clipping_algo_src/whitewash.py
+++ scripts/whitewashing_algo/src/clipping_algo_src/whitewash.py
@@ -2,38 +2,68 @@ from PIL import Image, ImageDraw
 import numpy as np
 import csv as csv
 
-#open dark-line coordinates
-file = csv.reader(open("D:\\Dropbox\\ankur\\pixel_tests\\test_dark_12072013.csv","rb"))	//read information saved by "find_dark_line.py"
-data = []
-for row in file:
-    data.append(row[1])
-#print data
+def whitewash(location,matraFile) :
+    
+    #open dark-line coordinates
+    file = csv.reader(open(location + matraFile,"rb"))	#read information saved by "find_dark_line.py"
+    data = []
+    for row in file:
+        data.append(row[1])
+    #print data
+    
+    #open image and convert to hsv
+    im = Image.open("D:\\Dropbox\\ankur\\pixel_tests\\test.png") #change this if using user input paths
+    if im.mode != "L" :
+        im2 = im.convert(mode = "L") 
+    else:
+        im2 = im
+    
+    line_color = 255	#color to use for washing over the pixel coordinates
+    nx,ny =  im2.size
+    count = 0
+    for y in data:
+        k = int(y)
+        for x in range(nx):
+            xy = (x,k)
+            im2.putpixel(xy,line_color)
+            im2.putpixel((x,int(y)+1),line_color)
+            im2.putpixel((x,int(y)+2),line_color)
+            im2.putpixel((x,int(y)-1),line_color) #padding  #NOTE : limits are to be set manually depending on the document
+    '''
+            im2.putpixel((x,30),line_color)
+            im2.putpixel((x,31),line_color)
+            im2.putpixel((x,32),line_color)
+            im2.putpixel((x,33),line_color)
+    '''									#additional padding, used while testing/debugging
+    
+    #save the new image file
+    im2.save("D:\\Dropbox\\ankur\\pixel_tests\\boundary_markers_12072013.png") #change this if using user input paths 
+    im2.show()
 
-#open image and convert to hsv
-im = Image.open("D:\\Dropbox\\ankur\\pixel_tests\\test.png")
-if im.mode != "L" :
-    im2 = im.convert(mode = "L") 
-else
-	im2 = im
-
-line_color = 255	#color to use for washing over the pixel coordinates
-nx,ny =  im2.size
-count = 0
-for y in data:
-    k = int(y)
-    for x in range(nx):
-        xy = (x,k)
-        im2.putpixel(xy,line_color)
-        im2.putpixel((x,int(y)+1),line_color)
-        im2.putpixel((x,int(y)+2),line_color)
-        im2.putpixel((x,int(y)-1),line_color) #padding  #NOTE : limits are to be set manually depending on the document
-'''
-        im2.putpixel((x,30),line_color)
-        im2.putpixel((x,31),line_color)
-        im2.putpixel((x,32),line_color)
-        im2.putpixel((x,33),line_color)
-'''									#additional padding, used while testing/debugging
-
-#save the new image file
-im2.save("D:\\Dropbox\\ankur\\pixel_tests\\boundary_markers_12072013.png")
-im2.show()
+if __name__ == '__main__':
+    
+    #default location
+    location  = "D:\\Dropbox\\ankur\\pixel_tests\\"  #file location
+    matraFile = "test_dark_12072013.csv"
+    
+    #user input for location
+    #if(len(sys.argv)!=5):
+    #    print "Usage: python read_image_info.py -location <location> -imFile <file name>"
+    #    print "Error : insufficient arguments"
+    #    exit()
+    #
+    #if(sys.argv[1]=="-location"):
+    #    location=sys.argv[2]
+    #else:
+    #    print "Usage: python read_image_info.py -location <location> -imFile <file name>"
+    #    print "Error : wrong location"
+    #    exit()
+    #
+    #if(sys.argv[3]=="-imFile"):
+    #    imFile=sys.argv[4]
+    #else:
+    #    print "Usage: python read_image_info.py -location <location> -imFile <file name>"
+    #    print "Error : wrong filename"
+    #    exit()
+    
+    whitewash(location,matraFile)
